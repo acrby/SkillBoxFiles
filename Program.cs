49f3ef6090f03cd50e5e01d402f620dfac889cea
D:\Repo <hudkou@hotmail.com>
2021-10-03 23:30:43 +0300

    FullyTested

--- Program.cs
+++ Program.cs
@@ -6,29 +6,68 @@ namespace SkillBoxFiles
 {
     class Program
     {
-        static void Main(string[] args)
+
+        /// <summary>
+        /// Самописный метод создания файла и его заполнения тестовыми данными
+        /// </summary>
+        /// <param name="filename">Имя файла, который будет создан в директории Решения</param>
+        static void CreateAndWriteFile(string filename)
         {
             #region Создание файла и запись
-            FileStream stream = File.Create("Test.csv");
-            stream.Flush();
+            FileStream stream = File.Create(filename);
+            stream.Close();
 
-            DateTime[] Dates = new DateTime[] { Convert.ToDateTime("21.03.20").ToShortDateString, Convert.ToDateTime("10.10.20"), Convert.ToDateTime("01.10.21") };  
-            Dates.
-            #endregion
+            DateTime[] dates = new DateTime[] { Convert.ToDateTime("21.03.20"), Convert.ToDateTime("10.10.20"), Convert.ToDateTime("01.10.21") };
+            string[] note = new string[] { "Сходить в магазин", "Поесть булки", "Перечислить деньги" };
+            bool[] warning = new bool[] { false, true, true };
+            string fullMark;
 
-
-
-
-            #region Чтение
-            using (StreamReader data = new StreamReader("Test.csv", Encoding.Unicode))
+            using (StreamWriter data = new StreamWriter(filename, true, Encoding.Unicode))
             {
-                while(!data.EndOfStream)
+                for (int index = 0; index < note.Length; index++)
                 {
-                    Console.WriteLine(data.ReadLine());
+                    fullMark = dates[index].ToShortDateString() + "\t" + note[index] + "\t" + warning[index] + "\t";
+                    data.WriteLine(fullMark);
                 }
 
             }
             #endregion
+        }
+
+        /// <summary>
+        /// Самописная функция чтения файла
+        /// </summary>
+        /// <param name="filename">Имя файла с расширением, которое будет прочитано в директории Решения</param>
+        static void ReadFile(string filename)
+        {
+            #region Чтение
+            using (StreamReader data = new StreamReader(filename, Encoding.Unicode))
+            {
+                string line;
+                while (!data.EndOfStream)
+                {
+                    while ((line = data.ReadLine()) != null)
+                    {
+                        string[] datastring = line.Split('\t');
+                        Console.WriteLine($"{datastring[0],-15} {datastring[1],-20} {datastring[2],7}");
+                    }
+                }
+
+            }
+            #endregion
+        }
+
+
+        /// <summary>
+        /// Ебучий метод Main
+        /// </summary>
+        /// <param name="args">Параметры которые залетают, их описание</param>
+        static void Main(string[] args)
+        {
+            string filename = "Test.xls";
+            CreateAndWriteFile(filename);
+            ReadFile(filename);
+
             Console.ReadKey();
         }
     }
