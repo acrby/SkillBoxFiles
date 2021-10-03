cc9d72530cacfe524fe7e98f0c23529c655687ee
D:\Repo <hudkou@hotmail.com>
2021-10-03 22:22:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SkillBoxFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Создание файла и запись
+            FileStream stream = File.Create("Test.csv");
+            stream.Flush();
+
+            DateTime[] Dates = new DateTime[] { Convert.ToDateTime("21.03.20").ToShortDateString, Convert.ToDateTime("10.10.20"), Convert.ToDateTime("01.10.21") };  
+            Dates.
+            #endregion
+
+
+
+
+            #region Чтение
+            using (StreamReader data = new StreamReader("Test.csv", Encoding.Unicode))
+            {
+                while(!data.EndOfStream)
+                {
+                    Console.WriteLine(data.ReadLine());
+                }
+
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
